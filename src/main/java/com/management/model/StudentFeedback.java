ab299656153613f7cdd736d9de3207afe3156633
Nikita Raghuwanshi <raghuvanshinikita212@gmail.com>
2024-08-15 17:08:15 +0530

    Add getter and setter methods to StudentFeedback.java

---
+++ src/main/java/com/management/model/StudentFeedback.java
@@ -0,0 +1,33 @@
+package model;
+public class StudentFeedback {
+    private String studentName;
+    private String studentEmail;
+    private String feedback;
+
+    // Getter and Setter methods for studentName
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    // Getter and Setter methods for studentEmail
+    public String getStudentEmail() {
+        return studentEmail;
+    }
+
+    public void setStudentEmail(String studentEmail) {
+        this.studentEmail = studentEmail;
+    }
+
+    // Getter and Setter methods for feedback
+    public String getFeedback() {
+        return feedback;
+    }
+
+    public void setFeedback(String feedback) {
+        this.feedback = feedback;
+    }
+}
