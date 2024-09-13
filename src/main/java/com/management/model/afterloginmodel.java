5bb16822c4575911860a5f3e4fda978c13fdfb8d
jayprakashojha <116810188+jayprakashojha@users.noreply.github.com>
2024-09-13 22:43:37 +0530

    new code

---
+++ src/main/java/com/management/model/afterloginmodel.java
@@ -0,0 +1,21 @@
+package com.management.controller;
+
+public class afterloginmodel {
+
+	public String name;
+
+		public String getName() {
+		return name;
+	}
+
+	public String setName(String name) {
+		return this.name = name;
+	}
+
+		@Override
+	public String toString() {
+		return "afterloginmodel [name=" + name + "]";
+	}
+	
+	
+}
