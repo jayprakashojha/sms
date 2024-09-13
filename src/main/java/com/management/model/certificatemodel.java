36f4dd4140161cf6121b4df6cc9a98a6ea2a6c17
jayprakashojha <116810188+jayprakashojha@users.noreply.github.com>
2024-09-13 22:45:00 +0530

    new code

---
+++ src/main/java/com/management/model/certificatemodel.java
@@ -0,0 +1,297 @@
+package com.management.controller;
+
+import java.util.Arrays;
+
+public class certificatemodel {
+
+	public String nurseryname;
+	public byte[] nursery;
+	public String lkgname;
+	public byte[] lkg;
+	public String ukgname;
+	public byte[] ukg;
+	public String firstname;
+	public byte[] first;
+	public String secondname;
+	public byte[] second;
+	public String thirdname;
+	public byte[] third;
+	public String fourthname;
+	public byte[] fourth;
+	public String fifthname;
+	public byte[] fifth;
+	public String sixname;
+	public byte[] six;
+	public String sevenname;
+	public byte[] seven;
+	public String eightname;
+	public  String getEightname() {
+		return eightname;
+	}
+
+	public  void setEightname(String eightname) {
+		this.eightname = eightname;
+	}
+
+	public byte[] eight;
+	public String nineyname;
+	public byte[] nine;
+	public String tenname;
+	public byte[] ten;
+	public String elevenname;
+	public byte[] eleven;
+	public String twelveyname;
+	public byte[] twelve;
+	
+	public  byte[] getTwelve() {
+		return twelve;
+	}
+
+	public  void setTwelve(byte[] twelve) {
+		this.twelve = twelve;
+	}
+
+	public  String getTwelveyname() {
+		return twelveyname;
+	}
+
+	public  void setTwelveyname(String twelveyname) {
+		this.twelveyname = twelveyname;
+	}
+
+	public  byte[] getEleven() {
+		return eleven;
+	}
+
+	public  void setEleven(byte[] eleven) {
+		this.eleven = eleven;
+	}
+
+	public  String getElevenname() {
+		return elevenname;
+	}
+
+	public  void setElevenname(String elevenname) {
+		this.elevenname = elevenname;
+	}
+
+	public  byte[] getTen() {
+		return ten;
+	}
+
+	public  void setTen(byte[] ten) {
+		this.ten = ten;
+	}
+
+	public  String getTenname() {
+		return tenname;
+	}
+
+	public  void setTenname(String tenname) {
+		this.tenname = tenname;
+	}
+
+	public  byte[] getNine() {
+		return nine;
+	}
+
+	public  void setNine(byte[] nine) {
+		this.nine = nine;
+	}
+
+	public  String getNineyname() {
+		return nineyname;
+	}
+
+	public  void setNineyname(String nineyname) {
+		this.nineyname = nineyname;
+	}
+
+	public  byte[] getEight() {
+		return eight;
+	}
+
+	public  void setEight(byte[] eight) {
+		this.eight = eight;
+	}
+
+	public  byte[] getSeven() {
+		return seven;
+	}
+
+	public  void setSeven(byte[] seven) {
+		this.seven = seven;
+	}
+
+	public  String getSevenname() {
+		return sevenname;
+	}
+
+	public  void setSevenname(String sevenname) {
+		this.sevenname = sevenname;
+	}
+
+	public  byte[] getSix() {
+		return six;
+	}
+
+	public  void setSix(byte[] six) {
+		this.six = six;
+	}
+
+	public  String getSixname() {
+		return sixname;
+	}
+
+	public  void setSixname(String sixname) {
+		this.sixname = sixname;
+	}
+
+	public  byte[] getFifth() {
+		return fifth;
+	}
+
+	public  void setFifth(byte[] fifth) {
+		this.fifth = fifth;
+	}
+
+	public  String getFifthname() {
+		return fifthname;
+	}
+
+	public  void setFifthname(String fifthname) {
+		this.fifthname = fifthname;
+	}
+
+	public  byte[] getFourth() {
+		return fourth;
+	}
+
+	public  void setFourth(byte[] fourth) {
+		this.fourth = fourth;
+	}
+
+	public  String getFourthname() {
+		return fourthname;
+	}
+
+	public  void setFourthname(String fourthname) {
+		this.fourthname = fourthname;
+	}
+
+	public  byte[] getThird() {
+		return third;
+	}
+
+	public  void setThird(byte[] third) {
+		this.third = third;
+	}
+
+	public  String getThirdname() {
+		return thirdname;
+	}
+
+	public  void setThirdname(String thirdname) {
+		this.thirdname = thirdname;
+	}
+
+	public  byte[] getSecond() {
+		return second;
+	}
+
+	public  void setSecond(byte[] second) {
+		this.second = second;
+	}
+
+	public  String getSecondname() {
+		return secondname;
+	}
+
+	public  void setSecondname(String secondname) {
+		this.secondname = secondname;
+	}
+
+	public  byte[] getFirst() {
+		return first;
+	}
+
+	public  void setFirst(byte[] first) {
+		this.first = first;
+	}
+
+	public  String getFirstname() {
+		return firstname;
+	}
+
+	public  void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public  byte[] getUkg() {
+		return ukg;
+	}
+
+	public  void setUkg(byte[] ukg) {
+		this.ukg = ukg;
+	}
+
+	public  String getUkgname() {
+		return ukgname;
+	}
+
+	public  void setUkgname(String ukgname) {
+		this.ukgname = ukgname;
+	}
+
+	public  byte[] getLkg() {
+		return lkg;
+	}
+
+	public  void setLkg(byte[] lkg) {
+		this.lkg = lkg;
+	}
+
+	public  String getLkgname() {
+		return lkgname;
+	}
+
+	public  void setLkgname(String lkgname) {
+		this.lkgname = lkgname;
+	}
+
+	public  byte[] getNursery() {
+		return nursery;
+	}
+
+	public  void setNursery(byte[] nursery) {
+		this.nursery = nursery;
+	}
+
+	public  String getNurseryname() {
+		return nurseryname;
+	}
+
+	public  void setNurseryname(String nurseryname) {
+		this.nurseryname = nurseryname;
+	}
+
+	@Override
+	public String toString() {
+		return "certificatemodel [nurseryname=" + nurseryname + ", nursery=" + Arrays.toString(nursery) + ", lkgname="
+				+ lkgname + ", lkg=" + Arrays.toString(lkg) + ", ukgname=" + ukgname + ", ukg=" + Arrays.toString(ukg)
+				+ ", firstname=" + firstname + ", first=" + Arrays.toString(first) + ", secondname=" + secondname
+				+ ", second=" + Arrays.toString(second) + ", thirdname=" + thirdname + ", third="
+				+ Arrays.toString(third) + ", fourthname=" + fourthname + ", fourth=" + Arrays.toString(fourth)
+				+ ", fifthname=" + fifthname + ", fifth=" + Arrays.toString(fifth) + ", sixname=" + sixname + ", six="
+				+ Arrays.toString(six) + ", sevenname=" + sevenname + ", seven=" + Arrays.toString(seven)
+				+ ", eightname=" + eightname + ", eight=" + Arrays.toString(eight) + ", nineyname=" + nineyname
+				+ ", nine=" + Arrays.toString(nine) + ", tenname=" + tenname + ", ten=" + Arrays.toString(ten)
+				+ ", elevenname=" + elevenname + ", eleven=" + Arrays.toString(eleven) + ", twelveyname=" + twelveyname
+				+ ", twelve=" + Arrays.toString(twelve) + "]";
+	}
+	
+	
+	
+	
+
+}
